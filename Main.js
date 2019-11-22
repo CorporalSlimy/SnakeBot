64b88a15b4ee0812809ad15ad37a984ec896a03c
CorporalSlimy <50500203+CorporalSlimy@users.noreply.github.com>
2019-11-21 21:44:11 -0500

    Add files via upload

--- Main.js
+++ Main.js
@@ -3,8 +3,18 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require(`./config/config.json`)
 const ytdl = require('ytdl-core')
+const enmap = require(`enmap`)
 const ffmpeg = require('ffmpeg-static')
+const bsqlite3 = require(`better-sqlite3`)
 console.log(`Loaded require Constants`)
+// enmap setup
+client.settings = new enmap({
+	name: `settings`,
+	fetchAll: false,
+	autoFetch: true,
+	cloneLevel: `deep`
+})
+console.log(`Loaded enmap settings`)
 // main.js constants
 const queue = new Map()
 console.log(`Loaded main.js Constants`)
@@ -14,28 +24,44 @@ var bot_secret_token = config.botToken
 var botName = config.botName
 var djRole = config.djRole
 var adminRole = config.administratorRole
+var devRole = config.snakeBotDevRole
 var volume = config.musicVol
 var p = config.commandPrefix
+var modRole = config.moderatorRole
+var welcomeChannel = config.welcomeChannel
+var welcomeMessage = config.welcomeMessage
+var modLogChannel = config.modLogChannel
 console.log(`Loaded config varibles`)
 // main.js varibles
 var songs = []
 var songQueue = []
 var queueLen = 0
 var songTitle = []
+var djRoleEnabled = true
 console.log(`Loaded main.js Varibles`)
 var helpArr = [
 	`\n ***Commands***`,
 	`\n ${p}play (YouTube link) : plays the song that is linked. Rquires role ${djRole}`,
 	`\n ${p}skip : skips the current playing song. Rquires role ${djRole}`,
-	`\n ${p}stop : stops all music(temp disabled). Rquires role ${djRole}`,
+	`\n ${p}stop : stops all music. Rquires role ${djRole}`,
 	`\n ${p}roll : rolls a 1d20`,
 	`\n ${p}roll XdX : replace X with a number, rolls a die based on specifcations`,
 	`\n ***Admin Commands***`,
-	`\n __*Requires role ${adminRole}*__`,
 	`\n ${p}help suffix : Change ${botName}'s suffix`,
-	`\n ${p}help volume : change the volume of the music bot(check for VC connection first)`
-	]
+	`\n ${p}help volume : change the volume of the music bot(check for VC connection first)`,
+	`\n ${p}setconf : alter the server configuration case sensitive`,
+	`\n ${p}showconf : show current server configuration case sensitive`
+]
 console.log(`Loaded Help Array`)
+const defaultSettings = {
+	commandPrefix: p,
+	modLogChannel: modLogChannel,
+	modRole: modRole,
+	adminRole: adminRole,
+	welcomeChannel: welcomeChannel,
+	welcomeMessage: welcomeMessage
+}
+console.log(`Loaded defaultSettings`)
 client.login(bot_secret_token)
 console.log(`Inserted Token`)
 client.on('ready', () => {
@@ -43,61 +69,111 @@ client.on('ready', () => {
 })
 client.on('error', console.error)
 
+client.on(`guildDelete`, guild => {
+	client.settings.delete(guild.id);
+	console.log(`Guild ${guild.id} was deleted.`)
+})
+
+client.on("guildMemberAdd", member => {
+	client.settings.ensure(member.guild.id, defaultSettings);
+	let welcomeMessage = client.settings.get(member.guild.id, "welcomeMessage");
+	let user = message.mentions.users.first()
+	welcomeMessage = welcomeMessage.replace(user, member.user.tag)
+	member.guild.channels
+	  .find("name", client.settings.get(member.guild.id, "welcomeChannel"))
+	  .send(welcomeMessage)
+	  .catch(console.error);
+  });
+
+
+
+
 client.on(`message`, async (message) => {
 
 	// Message Constants
 	const serverQueue = queue.get(message.guild.id)
-	const messageWords = message.content.split(' ')
+	const messageWords = message.content.toLowerCase().split(' ')
 	const rollFlavor = messageWords.slice(2).join(' ')
 	const member = message.mentions.members.first();
-
+	const guildConf = client.settings.ensure(message.guild.id, defaultSettings)
+	const args = message.content.split(/\s+/g);
+	const command = args.shift().slice(guildConf.commandPrefix.length).toLowerCase();
 	// MUSIC BOT check for commands
-	if (message.author.bot) return
-	if (messageWords[0] === `${p}play` && message.member.roles.some(role => role.name === djRole)) {
+	if (!message.guild || message.author.bot) return
+	if (command === "setconf") {
+
+		const adminRole = message.guild.roles.find("name", guildConf.adminRole);
+		const devRole = message.member.roles.some(role => role.name === djRole)
+		if (!adminRole) return message.reply("Administrator Role Not Found");
+
+		if (!message.member.roles.has(adminRole.id)) {
+			return message.reply("Insufficient permissions.");
+		}
+
+		const [prop, ...value] = args;
+		if (prop === `adminRole`) {
+
+		}
+		if (!client.settings.has(message.guild.id, prop)) {
+			return message.reply("This key is not in the configuration.");
+		}
+
+		client.settings.set(message.guild.id, value.join(" "), prop);
+		message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
+	}
+	if (messageWords[0] === `${p}showconf`) {
+		let configProps = Object.keys(guildConf).map(prop => {
+			return `\n ${prop}  :  ${guildConf[prop]}`;
+		})
+		message.channel.send(`The following are the server's current configuration:
+		\`\`\`${configProps}\`\`\``);
+	}
+
+
+
+
+	if (command === `play` && message.member.roles.some(role => role.name === djRole)) {
 		execute(message, serverQueue)
 		return
-	} else if (messageWords[0] === `${p}skip` && message.member.roles.some(role => role.name === djRole)) {
+	} else if (command === `skip` && message.member.roles.some(role => role.name === djRole)) {
 		skip(message, serverQueue);
 		return
-	} else if (messageWords[0] === `${p}stop` && message.member.roles.some(role => role.name === djRole)) {
+	} else if (command === `stop` && message.member.roles.some(role => role.name === djRole)) {
 		stop(message, serverQueue);
 		return
 		// return message.reply(`Feature temporarily disabled.`)
-	} else if (messageWords[0] === `${p}queue` && message.member.roles.some(role => role.name === djRole)) {
+	} else if (command === `queue` && message.member.roles.some(role => role.name === djRole)) {
 		musicQueue(message, serverQueue)
 		return
-	} else if ((messageWords[0] === `${p}play` || messageWords[0] === `${p}skip` || messageWords[0] === `${p}queue` || messageWords[0] === `${p}stop`) && !message.member.roles.some(role => role.name === djRole)) {
+	} else if ((command === `play` || command === `skip` || command === `queue` || command === `stop`) && !message.member.roles.some(role => role.name === djRole)) {
 		return message.reply(`Insuffienct permissions to Play,Skip,Stop, and view the queue.`)
 	}
 
+	// $volume
+	if (command === `volume` && message.member.roles.some(role => role.name === adminRole)) {
+		if (!isNaN(messageWords[1])) {
+			volume = messageWords[1]
+			return message.reply(`Volume has been set to ${volume}`)
+		} else if (isNaN(messageWords[2])) {
+			return message.reply(`Please enter a number`)
+		} else {
+			return message.reply(`Please enter a number`)
+		}
+	}
+
+	// $suffix
+	if (command === `suffix` && message.member.roles.some(role => role.name === adminRole)) {
+		p = messageWords[1]
+		return message.reply(`\nSuffix for ${botName} has been set to "${p}"`)
+	} else {
+		message.channel.send(`Insufficient permissons.`)
+	}
+
 	// chat bot
-	if (messageWords[0] === `${p}help`) {
+	if (command === `help`) {
 		if (messageWords.length === 1) {
 			// $help
-			// return message.reply(`\nCOMMANDS:\n ${p}play (YouTube link) : plays the song that is linked. \n ${p}skip : skips the current playing song. \n ${p}stop : stops all music(temp disabled). \n ${p}roll : rolls 1d6 \n ${p}roll XdX : replace X with a number, rolls a die based on specifcations \nADMIN COMMANDS: \n ${p}help suffix : Change the suffix \n ${p}help volume : change the volume of the music bot(check for VC connection first)`)
 			return message.channel.send(`${helpArr}`)
-		}
-		if (isNaN(messageWords[1][0]) && messageWords[1].includes(`volume`)) {
-			if (message.member.roles.some(role => role.name === adminRole)) {
-				if (!isNaN(messageWords[2])) {
-					volume = messageWords[2]
-					return message.reply(`Volume has been set to ${volume}`)
-				} else if (isNaN(messageWords[2])) {
-					return message.reply(`Please enter a number`)
-				} else {
-					return message.reply(`Please enter a number`)
-				}
-			} else {
-				return message.reply(`Insufficient permissions`)
-			}
-		}
-		if (isNaN(messageWords[1][0]) && messageWords[1].includes(`suffix`)) {
-			if (message.member.roles.some(role => role.name === adminRole)) {
-				p = messageWords[2]
-				return message.reply(`\nSuffix for ${botName} has been set to "${p}"`)
-			} else {
-				return message.reply(`Insufficient permissions`)
-			}
 		}
 	}
 
@@ -159,7 +235,7 @@ async function execute(message, serverQueue) {
 
 	const voiceChannel = message.member.voiceChannel;
 	if (!voiceChannel) return message.channel.send('Please enter a voice chat to play music.');
-	const permissions = voiceChannel.permissionsFor(message.client.user);
+	const permissions = voiceChannel.permissionsFor(client.message.user);
 	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
 		return message.channel.send('${botName} has insufficient permissions to play music.');
 	}
